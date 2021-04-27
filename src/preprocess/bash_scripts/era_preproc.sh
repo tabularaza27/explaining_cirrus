c11f30094e635345c55861c65b3090c91e49e710
tabularaza27 <kai.jeggle@gmx.de>
2021-04-27 11:16:48 +0200

    execute in parallel on threads

--- src/preprocess/bash_scripts/era_preproc.sh
+++ src/preprocess/bash_scripts/era_preproc.sh
@@ -10,6 +10,8 @@ Intermediate_File_Directory='/net/n2o/wolke_scratch/kjeggle/ERA5/intermediate'
 Preproc_File_Directory='/net/n2o/wolke_scratch/kjeggle/ERA5/preproc'
 Grid_Spec_Path='/home/kjeggle/cirrus/src/preprocess/bash_scripts/gridspec'
 
+Threads=8
+
 for filename in $(ls ${Source_File_Directory}/era5_date_*_time_*.grb)
 do
         d=`echo $filename | grep -E -o '[0-9]{4}_[0-9]{2}_[0-9]{2}'`
@@ -23,10 +25,10 @@ do
           continue
         fi
         # 1. Rename r,q,t parameters (needed for afterburner), convert spectral to gaussian grid, set from  reduced gaussian to regular grid type
-        cdo -f nc -chparam,0.0.0,130,0.1.0,133,25.3.0,152 -sp2gpl -setgridtype,regular $filename ${Intermediate_File_Directory}/era5_date_${d}_time_${t}.nc
+        cdo -P $Threads -f nc -chparam,0.0.0,130,0.1.0,133,25.3.0,152 -sp2gpl -setgridtype,regular $filename ${Intermediate_File_Directory}/era5_date_${d}_time_${t}.nc
 
         # 2. calculate rh with r,q,t via the cdo afterburner. Bash script Here document Limit string must be at beginning of line (https://tldp.org/LDP/abs/html/here-docs.html)
-        cdo after ${Intermediate_File_Directory}/era5_date_${d}_time_${t}.nc ${Intermediate_File_Directory}/rh_era5_date_${d}_time_${t}.nc << BREAK
+        cdo -P $Threads after ${Intermediate_File_Directory}/era5_date_${d}_time_${t}.nc ${Intermediate_File_Directory}/rh_era5_date_${d}_time_${t}.nc << BREAK
             CODE=157 TYPE=20
 BREAK
 
@@ -37,8 +39,8 @@ BREAK
         cdo -f nc -sellonlatbox,-180,180,-90,90 -random,r1440x720 template.nc # create target grid
         cdo setgrid,${Grid_Spec_Path} template.nc template.nc # template grid has an offset for some reason, force correct starting point
         cdo setgrid,mygrid template.nc template.nc # template grid has an offset for some reason, force correct starting point
-        cdo -remapcon,template.nc ${Intermediate_File_Directory}/rh_ncatted_era5_date_${d}_time_${t}.nc ${Intermediate_File_Directory}/rh_remap_era5_date_${d}_time_${t}.nc # rh is extensive variable: conservative interp
-        cdo -remapbil,template.nc ${Intermediate_File_Directory}/era5_date_${d}_time_${t}.nc ${Intermediate_File_Directory}/remap_era5_date_${d}_time_${t}.nc # other variables are intensive variables:  bilinear interp
+        cdo -P $Threads -remapcon,template.nc ${Intermediate_File_Directory}/rh_ncatted_era5_date_${d}_time_${t}.nc ${Intermediate_File_Directory}/rh_remap_era5_date_${d}_time_${t}.nc # rh is extensive variable: conservative interp
+        cdo -P $Threads -remapbil,template.nc ${Intermediate_File_Directory}/era5_date_${d}_time_${t}.nc ${Intermediate_File_Directory}/remap_era5_date_${d}_time_${t}.nc # other variables are intensive variables:  bilinear interp
 
         # 4. Select our target domain
         cdo sellonlatbox,$MIN_LON,$MAX_LON,$MIN_LAT,$MAX_LAT ${Intermediate_File_Directory}/rh_remap_era5_date_${d}_time_${t}.nc ${Intermediate_File_Directory}/rh_sellatlon_era5_date_${d}_time_${t}.nc
