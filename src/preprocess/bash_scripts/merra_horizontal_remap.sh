74132d64828835590b5dbcb40354ea9dcff976b0
tabularaza27 <kai.jeggle@gmx.de>
2021-05-19 10:35:48 +0200

    join temperature data in remapping step

--- src/preprocess/bash_scripts/merra_horizontal_remap.sh
+++ src/preprocess/bash_scripts/merra_horizontal_remap.sh
@@ -13,6 +13,7 @@ MAX_LAT=60
 
 Intermediate_File_Directory='/net/n2o/wolke_scratch/kjeggle/MERRA2/intermediate'
 Preproc_File_Directory='/net/n2o/wolke_scratch/kjeggle/MERRA2/preproc'
+Temperature_Directory='/net/n2o/wolke_scratch/kjeggle/MERRA2/temp_data/inst_incoming/'
 Grid_Spec_Path='/home/kjeggle/cirrus/src/preprocess/bash_scripts/gridspec'
 
 d=`echo $filename | grep -E -o '[0-9]{8}'`
@@ -25,14 +26,20 @@ if test -f "$FINAL_FILE"; then
   exit
 fi
 
+temperature_file=${Temperature_Directory}/MERRA2_400.inst3_3d_asm_Nv.${d}.nc4.nc4
+
 # 1. create template grid
 cdo -f nc -sellonlatbox,-180,180,-90,90 -random,r1440x720 template.nc # create target grid
 Grid_Spec_Path='/home/kjeggle/cirrus/src/preprocess/bash_scripts/gridspec'
 cdo setgrid,${Grid_Spec_Path} template.nc template.nc # template grid has an offset for some reason, force correct starting point
 cdo sellonlatbox,-75,-15,0,60 template.nc template.nc
 
-# 2. remap conservatively
-cdo remapcon,template.nc $filename $FINAL_FILE
+# 2. remap horizontally
+cdo remapcon,template.nc $filename ${Intermediate_File_Directory}/remap_merra2_date_${d}.nc
+cdo remapbil,template.nc $temperature_file ${Intermediate_File_Directory}/remap_temp_merra2_date_${d}.nc
+
+# 1. Join aerosol data with temperature data
+cdo merge ${Intermediate_File_Directory}/remap_merra2_date_${d}.nc ${Intermediate_File_Directory}/remap_temp_merra2_date_${d}.nc $FINAL_FILE
 
 # 6. Delete intermediate files
 # rm  ${Intermediate_File_Directory}/*_date_${d}_time_${t}.nc
