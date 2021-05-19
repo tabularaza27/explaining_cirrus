53c376a6176302580c4522203fd0ac539b8c8c25
tabularaza27 <kai.jeggle@gmx.de>
2021-05-19 12:44:40 +0200

    minor

--- src/preprocess/bash_scripts/create_template.sh
+++ src/preprocess/bash_scripts/create_template.sh
@@ -2,7 +2,7 @@
 # global grid with 0.25x0.25 resolution
 
 Grid_Spec_Path='/home/kjeggle/cirrus/src/config_files/gridspec'
-Template_Path='/home/kjeggle/cirrus/src/config_files/gridspec/template.nc'
+Template_Path='/home/kjeggle/cirrus/src/config_files/template.nc'
 
 cdo -f nc -sellonlatbox,-180,180,-90,90 -random,r1440x720 $Template_Path # create target grid
 cdo setgrid,${Grid_Spec_Path} $Template_Path $Template_Path # template grid has an offset for some reason, force correct starting point
