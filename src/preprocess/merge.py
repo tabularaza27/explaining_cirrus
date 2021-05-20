1b537008ade5068323037762541659d15e580d1a
tabularaza27 <kai.jeggle@gmx.de>
2021-05-20 18:32:52 +0200

    parallelized merging

--- src/preprocess/merge.py
+++ src/preprocess/merge.py
@@ -234,6 +234,8 @@ def run_merging(n_workers=4):
         # check if file already exists
         if exists(date, DATA_CUBE_FILESTUMPY, DESTINATION_DIR):
             print("File already exists for: {}".format(date))
+            continue
+
 
         pool.apply_async(merge_and_save, args=(date,))
 
