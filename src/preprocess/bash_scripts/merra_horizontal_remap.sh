2d835e236baaa86b9b31d9ad133d60edab32afd1
tabularaza27 <kai.jeggle@gmx.de>
2021-06-28 11:09:26 +0200

    merra quickfix

--- src/preprocess/bash_scripts/merra_horizontal_remap.sh
+++ src/preprocess/bash_scripts/merra_horizontal_remap.sh
@@ -26,11 +26,18 @@ if test -f "$FINAL_FILE"; then
   exit
 fi
 
-meteo_file=${Meteo_Directory}/MERRA2_400.inst3_3d_asm_Nv.${d}.nc4.nc4
+# Merra files are called different based on year see https://gmao.gsfc.nasa.gov/pubs/docs/Bosilovich785.pdf p.13
+year=`echo $filename | grep -E -o '[0-9]{4}'`
+if ((year<2011))
+then
+  meteo_file=${Meteo_Directory}/MERRA2_300.inst3_3d_asm_Nv.${d}.nc4.nc4
+else
+  meteo_file=${Meteo_Directory}/MERRA2_400.inst3_3d_asm_Nv.${d}.nc4.nc4
+fi
 
 # 1. remap horizontally
-cdo remapcon,$Template_Path $filename ${Intermediate_File_Directory}/remap_merra2_date_${d}.nc
 cdo remapbil,$Template_Path $meteo_file ${Intermediate_File_Directory}/remap_meteo_merra2_date_${d}.nc
+cdo remapcon,$Template_Path $filename ${Intermediate_File_Directory}/remap_merra2_date_${d}.nc
 
 # 2. select domain
 #cdo sellonlatbox,$MIN_LON,$MAX_LON,$MIN_LAT,$MAX_LAT ${Intermediate_File_Directory}/remap_merra2_date_${d}.nc ${Intermediate_File_Directory}/sel_remap_merra2_date_${d}.nc
