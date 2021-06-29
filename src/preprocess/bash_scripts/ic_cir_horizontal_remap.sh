ff502e5043f541610651bc5465e634e4b0a6cd18
tabularaza27 <kai.jeggle@gmx.de>
2021-06-29 14:32:58 +0200

    horizontal remap ic_cir all files of a year

--- src/preprocess/bash_scripts/ic_cir_horizontal_remap.sh
+++ src/preprocess/bash_scripts/ic_cir_horizontal_remap.sh
@@ -4,23 +4,27 @@
 # scripts that calls this bash scrip
 
 # filepath needs to be specified as first positional cmd argument when calling this file
-filename=$1
+year=$1
 
 echo $filename
 
+Source_File_Directory='/net/n2o/wolke_scratch/kjeggle/IC_CIR/incoming'
 Intermediate_File_Directory='/net/n2o/wolke_scratch/kjeggle/IC_CIR/intermediate'
 Template_Path='/home/kjeggle/cirrus/src/config_files/template.nc'
 
-year=`echo $filename | grep -E -o '[0-9]{4}' | tail -n1`
-day=`echo $filename | grep -E -o '[0-9]{3}' | tail -n1`
+mkdir -p ${Intermediate_File_Directory}/${year}
 
-echo "Start Horizontal remapping $year $day"
+for filename in $(ls ${Source_File_Directory}/${year}/*.nc)
+do
+  day=`echo $filename | grep -E -o '[0-9]{3}' | tail -n1`
+  echo "Start Horizontal remapping $year $day"
 
-FINAL_FILE=${Intermediate_File_Directory}/remapped_ic_cir_${year}_${day}.nc
-if test -f "$FINAL_FILE"; then
-  echo "Processed File already exists already exists."
-  exit
-fi
+  FINAL_FILE=${Intermediate_File_Directory}/${year}/remapped_ic_cir_${year}_${day}.nc
+  if test -f "$FINAL_FILE"; then
+    echo "Processed File already exists already exists."
+    exit
+  fi
 
-# 1. remap horizontally
-cdo remapnn,$Template_Path $filename $FINAL_FILE
+  # 1. remap horizontally
+  cdo remapnn,$Template_Path $filename $FINAL_FILE
+done
