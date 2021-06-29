32a8880b6c7d42739e89703186e0f14eabe924da
tabularaza27 <kai.jeggle@gmx.de>
2021-06-29 13:47:25 +0200

    horizontal remap ic_cir

--- src/preprocess/bash_scripts/ic_cir_horizontal_remap.sh
+++ src/preprocess/bash_scripts/ic_cir_horizontal_remap.sh
@@ -6,10 +6,12 @@
 # filepath needs to be specified as first positional cmd argument when calling this file
 filename=$1
 
+echo $filename
+
 Intermediate_File_Directory='/net/n2o/wolke_scratch/kjeggle/IC_CIR/intermediate'
 Template_Path='/home/kjeggle/cirrus/src/config_files/template.nc'
 
-year=`echo $filename | grep -E -o '[0-9]{4}'`
+year=`echo $filename | grep -E -o '[0-9]{4}' | tail -n1`
 day=`echo $filename | grep -E -o '[0-9]{3}' | tail -n1`
 
 echo "Start Horizontal remapping $year $day"
