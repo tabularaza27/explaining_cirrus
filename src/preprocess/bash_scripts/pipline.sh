05b582cf5aca8cc880396b9870f7cd61c2e6b946
tabularaza27 <kai.jeggle@gmx.de>
2021-07-02 16:53:49 +0200

    pipeline

--- src/preprocess/bash_scripts/pipline.sh
+++ src/preprocess/bash_scripts/pipline.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-conda activate cis_env
-python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "2007-01-01" "2007-12-31" 14
-conda activate xarray
-python /home/kjeggle/cirrus/src/preprocess/merge.py 7 2007
-python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2009
-python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2007
+/home/kjeggle/miniconda3/envs/cis_env/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "2007-01-01" "2007-12-31" 14
+/home/kjeggle/miniconda3/envs/xarray/python /home/kjeggle/cirrus/src/preprocess/merge.py 7 2007
+/home/kjeggle/miniconda3/envs/xarray/python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2009
+/home/kjeggle/miniconda3/envs/xarray/python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2007
 
 
 
