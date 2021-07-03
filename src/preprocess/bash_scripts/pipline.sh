f0a04a3ac2a29d32730f05757aa2847960b6fc0d
tabularaza27 <kai.jeggle@gmx.de>
2021-07-03 10:32:17 +0200

    pipeline

--- src/preprocess/bash_scripts/pipline.sh
+++ src/preprocess/bash_scripts/pipline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-/home/kjeggle/miniconda3/envs/cis_env/bin/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "2007-01-01" "2007-12-31" 14
+# /home/kjeggle/miniconda3/envs/cis_env/bin/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "2007-01-01" "2007-12-31" 14
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/era5_preproc_cdo.py 15 2007
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/merra2_preproc_cdo.py 15 2007
 /home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/merge.py 7 2007
 /home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2009
 /home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2007
