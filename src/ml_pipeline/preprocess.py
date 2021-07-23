81a9a2e02bfe81f82f4cd7d2e38e71c8829bca55
tabularaza27 <kai.jeggle@gmx.de>
2021-07-23 17:57:38 +0200

    added preprocessing functions

--- src/ml_pipeline/preprocess.py
+++ src/ml_pipeline/preprocess.py
@@ -24,19 +24,88 @@ def filter_df(df, filters):
     return f_df
 
 
-def oh_encoding(df, column_name):
-    """create one hot encoding for given column
+def oh_encoding(df, column_names, drop_original=True):
+    """create one hot encoding for given columns
 
     Args:
         df (pd.DataFrame):
-        column_name (str):
+        column_names (list):
+        drop_original (bool): if True, drop original column
 
     Returns:
         pd.DataFrame: dataframe with one hot encoding for given column. i.e. on column per feature value
 
     """
-    oh_df = pd.get_dummies(df[column_name], prefix=column_name)
-    df = pd.concat([df, oh_df], axis=1)
-    df = df.drop(column_name, axis=1)
+    for column_name in column_names:
+        oh_df = pd.get_dummies(df[column_name], prefix=column_name)
+        df = pd.concat([df, oh_df], axis=1)
+        if drop_original:
+            df = df.drop(column_name, axis=1)
+
+    return df
+
+
+def kickout_outliers(df, predictand, iqr_multiple=1.5):
+    """returns dataframe removed from outliers
+
+    Args:
+        df:
+        predictand (str): predictand where outlier calculation is based on
+        iqr_multiple (float): inter quartile range multiple. values outside iqr * multiple are considered outliers
+
+    Returns:
+
+    """
+    q25 = df[predictand].quantile(0.25)
+    q75 = df[predictand].quantile(0.75)
+
+    iqr = q75 - q25
+    cut_off = iqr * iqr_multiple
+    lower, upper = q25 - cut_off, q75 + cut_off
+
+    cutoff_idx = ~((df[predictand] < lower) | (df[predictand] > upper))
+    n_outliers = cutoff_idx[~cutoff_idx].count()
+    outlier_cleaned_df = df[cutoff_idx]
+
+    print("Original df: {} datapoints".format(df[df.columns[0]].count()))
+    print("kickout {} outliers".format(n_outliers))
+    print("New df: {} datapoints".format(outlier_cleaned_df[outlier_cleaned_df.columns[0]].count()))
+
+    return outlier_cleaned_df
+
+
+def log_transform(df, column_names, zero_handling="add_constant", drop_original=False):
+    """log transforms given columns of dataframe
+
+    Args:
+        df (pd.DataFrame):
+        column_names (list):
+        zero_handling (str): strategy to handle zero values, either `drop` or `add_constant`
+        drop_original (bool): if True, drop original column
+
+    Returns:
+        pd.DataFrame
+    """
+    assert zero_handling in ["add_constant", "drop"]
+
+    for col in column_names:
+
+        if df.query("{} == 0".format(col))[col].count() > 0:
+            print("{} contains zero values")
+            if zero_handling == "add_constant":
+                df["{}_log".format(col)] = (df[col] + 1e-25).transform(np.log)
+            elif zero_handling == "drop":
+                df = df.query("{} > 0".format(col))
+                df["{}_log".format(col)] = (df[col]).transform(np.log)
+
+        elif df.query("{} < 0".format(col))[col].count() > 0:
+            raise ValueError("{} contains negative values values")
+
+        else:
+            print("{} contains positive values only".format(col))
+            df["{}_log".format(col)] = (df[col]).transform(np.log)
+
+        if drop_original:
+            df.drop(col, axis=1, inplace=True)
 
     return df
