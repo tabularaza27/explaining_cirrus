b5dddf98fbeaf010427dcfeb09cc4ff05ed142ac
tabularaza27 <kai.jeggle@gmx.de>
2021-08-07 16:39:08 +0200

    bf

--- src/ml_pipeline/experiment.py
+++ src/ml_pipeline/experiment.py
@@ -1,10 +1,10 @@
 import sys
 import json
 import tempfile
+import comet_ml
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-import comet_ml
 import shap
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_squared_log_error, r2_score
