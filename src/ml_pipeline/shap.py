25fc2ce9b10ef987f256878d59acc8eb8a3eca1d
tabularaza27 <kai.jeggle@gmx.de>
2021-08-09 10:30:21 +0200

    create shap.py

---
+++ src/ml_pipeline/shap.py
@@ -0,0 +1,81 @@
+import tempfile
+import sys
+
+import shap
+import numpy as np
+import pandas as pd
+
+from src.preprocess.helpers.constants import TEMP_THRES
+from src.ml_pipeline.experiment import get_experiment_assets, load_experiment
+from src.ml_pipeline.ml_preprocess import create_dataset
+
+
+def calculate_and_log_shap_values(experiment_name, project_name, sample_size=None, log=True, interaction_values=False, check_additivity=True):
+    """return explainer object and shap values and index of predictions
+
+    Args:
+        experiment_name:
+        project_name:
+        sample_size (int|None): specifies how many rows are randomly selected to calculate shap values for
+        log (bool): If True, log to comet
+        interaction_values:
+
+    Returns:
+
+    """
+    experiment_config, xg_reg = get_experiment_assets(experiment_name, project_name)
+    experiment = load_experiment(experiment_name, project_name)
+
+    # load and create dataset
+    df = pd.read_pickle("/net/n2o/wolke/kjeggle/Notebooks/DataCube/df_pre_filtering.pickle")
+    # Drop NaN
+    df = df.dropna()
+    # Filter for Temperature
+    df = df.query("ta <= {}".format(TEMP_THRES))
+    X_train, X_val, X_test, y_train, y_val, y_test = create_dataset(df, **experiment_config)
+
+    if sample_size:
+        X_test = X_test.sample(n=sample_size, axis="index", random_state=123).sort_index()
+        print("Training set after sampling: {} Datapoints".format(X_test.t.count()))
+
+    # calculate shap_values
+    explainer = shap.TreeExplainer(xg_reg)
+    print("created explainer")
+    shap_values = explainer.shap_values(X_test, check_additivity=check_additivity)
+    print("calculated shap values")
+
+    if log:
+        # log shap values to experiment
+        fo = tempfile.NamedTemporaryFile(suffix=".npy")
+
+        with open(fo.name, "wb") as f:
+            np.save(f, shap_values)
+
+        experiment.log_asset(fo.name, ftype="shap_values")
+        fo.close()
+
+        # log indices of predictions for which shap values were calculated
+        fo = tempfile.NamedTemporaryFile(suffix=".npy")
+        print("saved shap values")
+
+        with open(fo.name, "wb") as f:
+            shap_idx = np.array(X_test.index)
+            np.save(f, shap_idx)
+
+        experiment.log_asset(fo.name, ftype="shap_idx")
+        fo.close()
+        print("saved shap indices")
+
+    return explainer, shap_values, shap_idx
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        experiment_names = list(sys.argv[1].split(','))
+    if len(sys.argv) == 3:
+        experiment_names = list(sys.argv[1].split(','))
+        sample_size = int(sys.argv[2])
+        for experiment_name in experiment_names:
+            print(experiment_name)
+        print(sample_size)
+    else:
+        raise ValueError("Provide valid arguments. E.g.: shap.py ['experiment_name','experiment_name'] <sample_size>")
