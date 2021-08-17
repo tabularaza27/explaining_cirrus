fc7f25ce7183eca495e30a0fc1d382da10dce495
tabularaza27 <kai.jeggle@gmx.de>
2021-08-17 13:59:32 +0200

    echam to datacube for inst variables

--- src/preprocess/bash_scripts/echam_to_data_cube.sh
+++ src/preprocess/bash_scripts/echam_to_data_cube.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 YEAR=2007
-SOURCE_DIR='/net/n2o/wolke_scratch/tullyc/cscs/project-temp/Kai/Raw'
-FILE_STUMPY='Kai_200701.01'
+#SOURCE_DIR='/net/n2o/wolke_scratch/tullyc/cscs/project-temp/Kai/Raw' # avg data
+SOURCE_DIR='/net/n2o/wolke_scratch/tullyc/cscs/project-temp/Kai_inst/Raw/' # instantaneous data
+
 INTERIM_DIR='/net/n2o/wolke_scratch/kjeggle/ECHAM/interim'
 DATACUBE_DIR='/net/n2o/wolke_scratch/kjeggle/ECHAM/datacube'
 
+# define domai
+MIN_LON=-75
+MAX_LON=-15
+MIN_LAT=0
+MAX_LAT=60
+
 # Define Variables of interest
 REMOS_VARS=drieff,dupdraft,dnihom,dnidet,dninuc,dninuc_hom,dninuc_het,dninuc_dust
-ECHAM_VARS=relhum,xi,aclcac,aclcov
+ECHAM_VARS=relhum,xi,aclcac,aclcov,geosp
 TRACER_VARS=SO4_AS,SO4_CS,SO4_KS,SO4_NS,DU_AI,DU_AS,DU_CI,DU_CS
 CIRRUS_VARS=homoAeroAllConc,dustImmAeroAllConc,dustDepAeroAllConc,sat
-ACTIV_VARS=W_LARGE,W
+ACTIV_VARS=W_LARGE,W,ICNC,ICNC_instantaneous,IWC,CLOUD_TIME,CLIWC_TIME,SICE
 
 for i in {01..12};
 do
-  FILE_STUMPY=Kai_${YEAR}${i}.01
+  FILE_STUMPY=Kai_inst_${YEAR}${i}.01
   echo "${FILE_STUMPY}"
 
   # temperature: convert spectral to gaussian grid
@@ -32,6 +39,7 @@ do
 
   # merge to monthly datacube file
   cdo merge ${INTERIM_DIR}/${FILE_STUMPY}_remos.nc ${INTERIM_DIR}/${FILE_STUMPY}_echam.nc ${INTERIM_DIR}/${FILE_STUMPY}_tracer.nc ${INTERIM_DIR}/${FILE_STUMPY}_cirrus.nc ${INTERIM_DIR}/${FILE_STUMPY}_activ.nc ${INTERIM_DIR}/${FILE_STUMPY}_echam_st.nc ${INTERIM_DIR}/${FILE_STUMPY}_echam_uv.nc ${DATACUBE_DIR}/${FILE_STUMPY}_datacube.nc
+  cdo sellonlatbox,$MIN_LON,$MAX_LON,$MIN_LAT,$MAX_LAT ${DATACUBE_DIR}/${FILE_STUMPY}_datacube.nc ${DATACUBE_DIR}/${FILE_STUMPY}_datacube_domain.nc
 
 done
 
