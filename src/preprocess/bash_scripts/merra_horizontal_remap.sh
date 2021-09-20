93243f426e1fc2607e6b0cebbc99363586fc173d
tabularaza27 <kai.jeggle@gmx.de>
2021-09-20 17:16:42 +0200

    refactor merra_horizontal_remap.sh

--- src/preprocess/bash_scripts/merra_horizontal_remap.sh
+++ src/preprocess/bash_scripts/merra_horizontal_remap.sh
@@ -6,15 +6,22 @@
 # filepath needs to be specified as first positional cmd argument when calling this file
 filename=$1
 
-MIN_LON=-75
-MAX_LON=-15
-MIN_LAT=0
-MAX_LAT=60
+# config_id needs to be specified as second positional cmd argument when calling this file
+# config id specifies the horizontal resolution and directories
+config_id=$2
 
-Intermediate_File_Directory='/net/n2o/wolke_scratch/kjeggle/MERRA2/intermediate'
-Preproc_File_Directory='/net/n2o/wolke_scratch/kjeggle/MERRA2/preproc'
-Meteo_Directory='/net/n2o/wolke_scratch/kjeggle/MERRA2/meteo_data/inst_incoming/' # temperature and geopotential height
-Template_Path='/home/kjeggle/cirrus/src/config_files/template.nc'
+# get directories for given config
+Meteo_Directory=`python -c "from src.scaffolding.scaffolding import get_data_product_dir; from src.preprocess.helpers.constants import MERRA_METEO_INCOMING_DIR; dir=get_data_product_dir('${config_id}', MERRA_METEO_INCOMING_DIR); print(dir)"`
+Intermediate_File_Directory=`python -c "from src.scaffolding.scaffolding import get_data_product_dir; from src.preprocess.helpers.constants import MERRA_IM_DIR; dir=get_data_product_dir('${config_id}', MERRA_IM_DIR); print(dir)"`
+Preproc_File_Directory=`python -c "from src.scaffolding.scaffolding import get_data_product_dir; from src.preprocess.helpers.constants import MERRA_PRE_PROC_DIR; dir=get_data_product_dir('${config_id}', MERRA_PRE_PROC_DIR); print(dir)"`
+Template_Path=`python -c "from src.scaffolding.scaffolding import get_abs_file_path; from src.preprocess.helpers.constants import TEMPLATE_PATH; path=get_abs_file_path('${config_id}', TEMPLATE_PATH); print(path)"`
+Config_File_Path=`python -c "from src.preprocess.helpers.constants import CONFIGS; print(CONFIGS)"`
+
+# get horizontal extent from config file (configs.json) for given config_id
+MIN_LON=`jq -r ".${config_id}.lonmin" $Config_File_Path`
+MAX_LON=`jq -r ".${config_id}.lonmax" $Config_File_Path`
+MIN_LAT=`jq -r ".${config_id}.latmin" $Config_File_Path`
+MAX_LAT=`jq -r ".${config_id}.latmax" $Config_File_Path`
 
 d=`echo $filename | grep -E -o '[0-9]{8}'`
 
@@ -46,5 +53,6 @@ cdo remapcon,$Template_Path $filename ${Intermediate_File_Directory}/remap_merra
 # 3. Join aerosol data with temperature data
 cdo merge ${Intermediate_File_Directory}/remap_merra2_date_${d}.nc ${Intermediate_File_Directory}/remap_meteo_merra2_date_${d}.nc $FINAL_FILE
 
+# todo delete intermediate directories
 # 6. Delete intermediate files
-# rm  ${Intermediate_File_Directory}/*_date_${d}_time_${t}.nc
+ rm  ${Intermediate_File_Directory}/*_date_${d}_time_${t}.nc
