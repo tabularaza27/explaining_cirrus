7172d8cdb2eae1e62c3b94a79b0ce761447a3425
tabularaza27 <kai.jeggle@gmx.de>
2021-11-04 11:46:43 +0100

    dardar l2 feature engineering + vertical regrid

---
+++ src/preprocess/dardar_l2_vertical_regrid.py
@@ -0,0 +1,171 @@
+"""feature engineering and vertical regrid for dardar l2 data"""
+
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+from src.preprocess.helpers.common_helpers import custom_mode
+from src.preprocess.helpers.constants import TEMP_THRES
+
+CAT_VARIABLES = ['clm', 'clm_v2', 'instrument_flag']  # use value with max occurance
+CONT_VARIABLES = [
+    'plev',
+    'ta',
+    'iwc',
+    'iwc_error',
+    'reffcli',
+    'icnc_5um',
+    'icnc_100um',
+    'icnc_5um_error',
+    'icnc_100um_error',
+    'dz_top',
+    'dz_top_v2',
+    'cloud_thickness',
+    'cloud_cover',
+    'cloud_layer']  # use mean
+FLAG_VARIABLES = ['cloud_top', 'cloud_bottom', 'liquid_origin', 'cloud_layer']  # take max
+
+
+def l2_feature_engineering(ds):
+    """
+
+    Args:
+        ds (xr.Dataset): L2 dataset of Dardar nice
+
+    Returns:
+        xr.Dataset: feature engineered L2 dataset
+    """
+    # define cloud masks
+    clm_v2_cloud_masks = [1, 2, 3, 4, 9,
+                          10]  # 3,4, contains super cooled water, is used for calculating cloud thickness and dz_top; will be filtered out later when filtering for t thres
+    # clm_v2_ice_masks = [1, 2, 9, 10]
+
+    # kick everything out that is above melting layer (tip from Odran → see his email)
+    melting_layer = 270
+    ds = ds.where(ds.ta < melting_layer)  # kickout everything above melting layer
+
+    # fill missing values
+    ds["clm_v2"] = ds.clm_v2.fillna(0)  # fill na values with clear sky
+
+    # cloud cover, if clm v2 has a cloud flag
+    ds["cloud_cover"] = (ds.clm_v2.isin(clm_v2_cloud_masks)).astype(int)
+
+    # flag layers that are at the top of a cloud (a nwe cloud layer starts if gap is more than 400m)
+    ds["cloud_free"] = ds.cloud_cover.where(ds.cloud_cover == 0) + 1
+    ds["cum_layer_distance"] = ds["cloud_free"].cumsum("height") - ds.cloud_free.cumsum("height").where(
+        ds.cloud_free.isnull()).ffill("height").fillna(0).astype(int)
+    ds["cum_layer_distance_diff"] = ds.cum_layer_distance.diff("height")
+    ds["cloud_top"] = (ds.cum_layer_distance_diff <= -7).astype(
+        int)  # identify cloud top, if layers are more then 7 layers apart (7*60 m)
+    ds["first_cloud_top"] = (ds.cloud_cover.where(ds.cloud_cover == 1).idxmax(dim="height") == ds.height).astype(int)
+    ds["cloud_top"] = (ds["first_cloud_top"] | ds["cloud_top"]).astype(int)
+
+    # flag cloud bottom, → same procedure as for cloud top with reversed index
+    ds_reindex = ds.cloud_free.reindex(height=ds.height[::-1])
+    cum_layer_distance = ds_reindex.cumsum("height") - ds_reindex.cumsum("height").where(ds_reindex.isnull()).ffill(
+        "height").fillna(0).astype(int)
+    cum_layer_distance_diff = cum_layer_distance.diff("height")
+    cloud_bottom = cum_layer_distance_diff <= -7
+    cloud_bottom = cloud_bottom.reindex(height=cloud_bottom.height[::-1])
+    ds["cloud_bottom"] = cloud_bottom.astype(int)
+
+    # enumerate cloud layers
+    ds["cloud_layer"] = ds.cloud_top.cumsum(dim="height").where(ds.cloud_cover == 1)
+
+    # calculate distance to cloud top based on clm v2
+    a = (ds.height * ds.cloud_top + ds.cloud_cover - ds.cloud_top)
+    cloud_top_height = a.where((a > 1)).ffill(dim="height") * ds.cloud_cover  # cloud top height for every layer
+    dz_top = (cloud_top_height - ds.height + 60) * ds.cloud_cover  # cloud top height - height of layer
+    ds["dz_top_v2"] = dz_top.where(dz_top)
+
+    # set iwc values to nan when clm_v2 == 1 and iwc == 0; iwc of layer will be discarded for mean iwc, icnc_5um, icnc_100um, reffcli, but still contributes to cloud_cover
+    # Reason for this phenomenon is that retrievals were calculated based on clm v1.
+    cloud_no_iwc = ds.cloud_cover.where((ds.cloud_cover == 1) & (ds.iwc > 0))
+    cirrus_properties = ["iwc", "icnc_5um", "icnc_100um", "reffcli", "iwc_error", "icnc_5um_error", "icnc_100um_error"]
+
+    for prop in cirrus_properties:
+        attrs = ds[prop].attrs
+        ds[prop] = ds[prop] * cloud_no_iwc
+        ds[prop].attrs = attrs
+
+    # calculate cloud thickness
+    ds["cloud_thickness"] = ds.dz_top_v2.where(ds.cloud_bottom == 1).bfill("height").where(ds.cloud_cover == 1)
+
+    # flag clouds that extend beyond cirrus regime temps
+    ds["liquid_origin"] = (ds.cloud_bottom - ds.cloud_bottom.where(ds.ta < TEMP_THRES, other=0)).where(
+        ds.cloud_bottom == 1).bfill("height").where(ds.cloud_cover == 1)
+
+    # set values for flag variables to nan where I dont have observations. Needed for horizontal aggregation later
+    # resulting an an in-cloud instrument flag variable
+    ds["instrument_flag"] = ds["instrument_flag"].where(ds.cloud_cover == 1)
+    ds["cloud_top"] = ds["cloud_top"].where(ds.cloud_cover == 1)
+    ds["cloud_bottom"] = ds["cloud_bottom"].where(ds.cloud_cover == 1)
+
+    # transpose dimens
+    ds = ds.transpose("time", "height")
+
+    return ds
+
+
+def l2_vertical_regrid(ds, layer_thickness):
+    """aggregates l2 data variables to new layerthickness
+
+    Args:
+        ds:
+        layer_thickness:
+
+    Returns:
+
+    """
+
+    agg_layers = layer_thickness / 60
+    assert agg_layers % 1 == 0, "layer_thickness must be multiple of 60, is {}".format(layer_thickness)
+
+
+    # mean for continous variables
+    coarse_cont = ds[CONT_VARIABLES].coarsen(height=agg_layers, boundary="trim", side="right", coord_func="mean").mean(
+        keep_attrs=True)
+    print("cont coarse")
+
+    # mode for categorical variables
+    coarse_cat = ds[CAT_VARIABLES].coarsen(height=agg_layers, boundary="trim", side="right", coord_func="mean").reduce(
+        custom_mode, keep_attrs=True)
+    print("cat coarse")
+
+    # max for flag variables
+    coarse_flag = ds[FLAG_VARIABLES].coarsen(height=agg_layers, boundary="trim", side="right", coord_func="mean").max(
+        keep_attrs=True)
+    print("flag coarse")
+
+    merge = xr.merge([coarse_cat, coarse_cont, coarse_flag])
+    print("merged")
+
+    # flag columns with clouds
+    merge["data_mask"] = (merge.cloud_cover.sum(dim="height") > 0).astype(int)
+
+    return merge
+
+
+def l2_filter_cirrus_regime(ds, altmax, altmin):
+    # all values for all variables will be set to nan
+    # + select only viable altitude range
+    ice = ds.sel(height=slice(altmax, altmin)).where(ds.ta <= TEMP_THRES)
+
+    # for cloud cover replace nan with 0 → needed for horizontal aggregation
+    ice["cloud_cover"] = ice.cloud_cover.fillna(0)
+
+    return ice
+
+
+def run_l2_preproc(ds, altmax, altmin, layer_thickness):
+    one_dim_vars = [var for var in ds.data_vars if len(ds[var].dims) == 1]
+    two_dim_vars = [var for var in ds.data_vars if len(ds[var].dims) == 2]
+
+    one_dim_ds = ds[one_dim_vars]
+
+    fe_ds = l2_feature_engineering(ds[two_dim_vars])
+    ice = l2_filter_cirrus_regime(fe_ds, altmax, altmin)
+    ds_coarse = l2_vertical_regrid(ice, layer_thickness)
+    merge = xr.merge([ds_coarse, one_dim_ds])
+
+    return merge
--- src/preprocess/helpers/common_helpers.py
+++ src/preprocess/helpers/common_helpers.py
@@ -1,5 +1,6 @@
 """Collection of common preprocessing helpers"""
 
+import numpy as np
 
 def check_for_nans(ds):
     """Checks for each variable in data set if it contains nan values and raises Error if it does so"""
@@ -11,3 +12,64 @@ def check_for_nans(ds):
             raise ValueError("{} contains nan values".format(var))
 
     # print("All Good - No nan values detected in this dataset")
+
+
+def custom_mode(ndarray, axis):
+    """calculates mode along np array
+
+    source: https://newbedev.com/most-efficient-way-to-find-mode-in-numpy-array
+    the scipy.stats.mode has the same functionality but is way slower
+    """
+
+    if type(axis) == tuple or type(axis) == list:
+        axis = axis[0]
+    # Check inputs
+    ndarray = np.asarray(ndarray)
+    ndim = ndarray.ndim
+    if ndarray.size == 1:
+        return (ndarray[0], 1)
+    elif ndarray.size == 0:
+        raise Exception('Cannot compute mode on empty array')
+    try:
+        axis = range(ndarray.ndim)[axis]
+    except:
+        raise Exception('Axis "{}" incompatible with the {}-dimension array'.format(axis, ndim))
+
+    # If array is 1-D and np version is > 1.9 np.unique will suffice
+    if all([ndim == 1,
+            int(np.__version__.split('.')[0]) >= 1,
+            int(np.__version__.split('.')[1]) >= 9]):
+        modals, counts = np.unique(ndarray, return_counts=True)
+        index = np.argmax(counts)
+        return modals[index], counts[index]
+
+    # Sort array
+    sort = np.sort(ndarray, axis=axis)
+    # Create array to transpose along the axis and get padding shape
+    transpose = np.roll(np.arange(ndim)[::-1], axis)
+    shape = list(sort.shape)
+    shape[axis] = 1
+    # Create a boolean array along strides of unique values
+    strides = np.concatenate([np.zeros(shape=shape, dtype='bool'),
+                              np.diff(sort, axis=axis) == 0,
+                              np.zeros(shape=shape, dtype='bool')],
+                             axis=axis).transpose(transpose).ravel()
+    # Count the stride lengths
+    counts = np.cumsum(strides)
+    counts[~strides] = np.concatenate([[0], np.diff(counts[~strides])])
+    counts[strides] = 0
+    # Get shape of padded counts and slice to return to the original shape
+    shape = np.array(sort.shape)
+    shape[axis] += 1
+    shape = shape[transpose]
+    slices = [slice(None)] * ndim
+    slices[axis] = slice(1, None)
+    # Reshape and compute final counts
+    counts = counts.reshape(shape).transpose(transpose)[slices] + 1
+
+    # Find maximum counts and return modals/counts
+    slices = [slice(None, i) for i in sort.shape]
+    del slices[axis]
+    index = np.ogrid[slices]
+    index.insert(axis, np.argmax(counts, axis=axis))
+    return sort[index]  # , counts[index]
