a83fe5c7ff6bfcd7955f2769e1ca57c198b08f36
tabularaza27 <kai.jeggle@gmx.de>
2021-11-12 17:06:14 +0100

    pipeline old

--- src/preprocess/bash_scripts/pipline.sh
+++ src/preprocess/bash_scripts/pipline.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 CONFIG_ID=$1
-YEAR=$2
-WORKER=$3
-/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "${YEAR}-01-01" "${YEAR}-12-31" "$CONFIG_ID" "$WORKER"
-/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "2007-01-01" "2007-12-31" "$CONFIG_ID" "$WORKER"
-#/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/era5_preproc_cdo.py "$WORKER" "$YEAR" "$CONFIG_ID"
-#/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/merra2_preproc_cdo.py "$WORKER" "$YEAR" "$CONFIG_ID"
-#/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/merge.py "$CONFIG_ID" "$WORKER" "$YEAR"
-#/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py "$YEAR" data # todo data cube filters has to be refactored
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "2008-01-01" "2008-12-31" $CONFIG_ID 4
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/era5_preproc_cdo.py 4 2008 $CONFIG_ID
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/merra2_preproc_cdo.py 4 2008 $CONFIG_ID
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/merge.py $CONFIG_ID 4 2008
+#/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2007 data # todo data cube filters has to be refactored
 
 
 
