689343f6545882d5c28d6de04108645b0574bb0a
tabularaza27 <kai.jeggle@gmx.de>
2021-11-12 16:48:56 +0100

    pipeline flexible execution

--- src/preprocess/bash_scripts/pipline.sh
+++ src/preprocess/bash_scripts/pipline.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 CONFIG_ID=$1
-/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "2008-01-01" "2008-12-31" $CONFIG_ID 4
-/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/era5_preproc_cdo.py 4 2008 $CONFIG_ID
-/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/merra2_preproc_cdo.py 4 2008 $CONFIG_ID
-/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/merge.py $CONFIG_ID 4 2008
+YEAR=$2
+WORKER=$3
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/dardar_nice_grid.py "${YEAR}-01-01" "${YEAR}-12-31" $CONFIG_ID
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/era5_preproc_cdo.py $WORKER $YEAR $CONFIG_ID
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/parallelize_bash_scripts/merra2_preproc_cdo.py $WORKER $YEAR $CONFIG_ID
+/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/merge.py $CONFIG_ID $WORKER $YEAR
 #/home/kjeggle/miniconda3/envs/xarray/bin/python /home/kjeggle/cirrus/src/preprocess/data_cube_filters.py 2007 data # todo data cube filters has to be refactored
 
 
