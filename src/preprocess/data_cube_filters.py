0da56da8619becc10352089b6d24f6ae684e5417
tabularaza27 <kai.jeggle@gmx.de>
2021-11-14 19:19:48 +0100

    datacube filters refactored to run with config

--- src/preprocess/data_cube_filters.py
+++ src/preprocess/data_cube_filters.py
@@ -6,9 +6,8 @@ import sys
 import glob
 import dask
 
-from src.preprocess.helpers.constants import TEMP_THRES, CLM_V2_ICE_CLOUD_MASKS
-
 from src.preprocess.helpers.constants import DATA_CUBE_PRE_PROC_DIR, DATA_CUBE_DF_DIR, DATA_CUBE_FILTERED_DIR
+from src.scaffolding.scaffolding import get_data_product_dir
 
 VAR_PROPERTIES = {
     'iwc': {'scale': 'lin', 'source': 'dardar', 'type': 'cirrus_var', 'load': True},
@@ -36,7 +35,6 @@ VAR_PROPERTIES = {
     'SO2': {'scale': 'lin', 'source': 'merra2', 'type': 'driver', 'load': True},
 }
 
-# todo refactor to be runnable with configs.json
 
 def get_load_variables():
     """return list of variables to load based on VAR_PROPERTIES"""
@@ -48,14 +46,24 @@ def get_drop_vars():
     return [var for var in VAR_PROPERTIES if VAR_PROPERTIES[var]["load"] == False]
 
 
-def get_month_files(year, month):
-    """return list of files for given month and year of datacube preproc"""
+def get_month_files(year, month, config_id):
+    """return list of files for given month and year of datacube preproc
+
+    Args:
+        year:
+        month:
+        config_id:
+
+    Returns:
+        list: list of file paths
+    """
     month = str(month).zfill(2)
-    files = glob.glob("{}/data_cube_perproc_{}_{}*.nc".format(DATA_CUBE_PRE_PROC_DIR, year, month))
+    data_cube_pre_proc_dir = get_data_product_dir(config_id, DATA_CUBE_PRE_PROC_DIR)
+    files = glob.glob("{}/data_cube_perproc_{}_{}*.nc".format(data_cube_pre_proc_dir, year, month))
     return files
 
 
-def filter_and_save_months(year, months, filter_type):
+def filter_and_save_months(year, months, filter_type, config_id):
     """create data frame and filtered dataset for given year and months. Filters data based on observation_mask or data_mask.
 
     observation_mask: True, where we have dardar retrievals
@@ -65,6 +73,7 @@ def filter_and_save_months(year, months, filter_type):
         year (int):
         months (list[int]): e.g. [1,2,3]
         filter_type (str): one of the following ['data','observations','observation_vicinity']
+        config_id (str) config determines resolutions and location of load/save directories
 
     Returns:
 
@@ -73,21 +82,26 @@ def filter_and_save_months(year, months, filter_type):
 
     initial_month_str = str(months[0]).zfill(2)
 
+    data_cube_df_dir = get_data_product_dir(config_id, DATA_CUBE_DF_DIR)
+    data_cube_filtered_dir = get_data_product_dir(config_id, DATA_CUBE_FILTERED_DIR)
+
     if filter_type == "data":
-        df_filename = "{}/ice_in_cloud_df_{}_{}.pickle".format(DATA_CUBE_DF_DIR, year, initial_month_str)
-        filtered_cube_filename = "{}/data_only_{}_{}.pickle".format(DATA_CUBE_FILTERED_DIR, year, initial_month_str)
+        df_filename = "{}/ice_in_cloud_df_{}_{}.pickle".format(data_cube_df_dir, year, initial_month_str)
+        filtered_cube_filename = "{}/data_only_{}_{}.pickle".format(data_cube_filtered_dir, year, initial_month_str)
         mask_var = "data_mask"
     elif filter_type == "observations":
-        df_filename = "{}/observations_df_{}_{}.pickle".format(DATA_CUBE_DF_DIR, year, initial_month_str)
-        filtered_cube_filename = "{}/observations_{}_{}.pickle".format(DATA_CUBE_FILTERED_DIR, year, initial_month_str)
+        df_filename = "{}/observations_df_{}_{}.pickle".format(data_cube_df_dir, year, initial_month_str)
+        filtered_cube_filename = "{}/observations_{}_{}.pickle".format(data_cube_filtered_dir, year, initial_month_str)
         mask_var = "observation_mask"
     elif filter_type == "observation_vicinity":
-        df_filename = "{}/observation_vicinity_df_{}_{}.pickle".format(DATA_CUBE_DF_DIR, year, initial_month_str)
-        filtered_cube_filename = "{}/observation_vicinity_{}_{}.pickle".format(DATA_CUBE_FILTERED_DIR, year, initial_month_str)
+        df_filename = "{}/observation_vicinity_df_{}_{}.pickle".format(data_cube_df_dir, year, initial_month_str)
+        filtered_cube_filename = "{}/observation_vicinity_{}_{}.pickle".format(data_cube_filtered_dir, year,
+                                                                               initial_month_str)
         mask_var = "observation_vicinity_mask"
     else:
         raise ValueError(
-            "filter_type needs to be one of the following ['data','observations','observation_vicinity'], was {}".format(filter_type))
+            "filter_type needs to be one of the following ['data','observations','observation_vicinity'], was {}".format(
+                filter_type))
 
     # check if file already exists
     if len(glob.glob(df_filename)) > 0:
@@ -97,7 +111,7 @@ def filter_and_save_months(year, months, filter_type):
     # get data cube files
     files = []
     for month in months:
-        files += get_month_files(year, month)
+        files += get_month_files(year, month, config_id)
 
     # load into xarray
     drop_vars = get_drop_vars()
@@ -106,7 +120,6 @@ def filter_and_save_months(year, months, filter_type):
                                parallel=True,
                                concat_dim="time",
                                drop_variables=drop_vars,
-                               chunks={'lat': 241, 'lon': 241, 'time': 24},
                                # Attempt to auto-magically combine the given datasets into one by using dimension
                                # coordinates.
                                combine="by_coords",
@@ -138,12 +151,12 @@ def filter_and_save_months(year, months, filter_type):
     df["hour"] = df.time.dt.hour
     df["month"] = df.time.dt.month
     df["season"] = ((df.time.dt.month % 12 + 3) // 3).map({1: 'DJF', 2: 'MAM', 3: 'JJA', 4: 'SON'})
-    drop = ["lev_2", "observation_mask", "data_mask", "observation_vicinity_mask", "timestep_observation_mask"]
+    drop = ["lev_2", "observation_mask", "data_mask"]
     df.drop(columns=drop, inplace=True)
 
     if filter_type == "data":
-        # filter data frame for cloud masks v1 and v2
-        df = df[(df.clm_v2.isin(CLM_V2_ICE_CLOUD_MASKS)) | (df.clm == 1)]
+        # filter for cloud cover
+        df = df[df.cloud_cover > 0]
 
     # write dataframe and data only ds to pickle
     df.to_pickle(df_filename)
@@ -152,10 +165,11 @@ def filter_and_save_months(year, months, filter_type):
         pickle.dump(filtered_cube, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def run_year(year, filter_type):
+def run_year(config_id, year, filter_type):
     """create data frame and data_only dataset for given year. one file each 3 months
 
     Args:
+        config_id (str) config determines resolutions and location of load/save directories
         year:
         filter_type (str): one of the following ['data','observations']
 
@@ -173,11 +187,11 @@ def run_year(year, filter_type):
             dashboard_port))
 
         for month_range in month_ranges:
-            filter_and_save_months(year, month_range, filter_type)
+            filter_and_save_months(year, month_range, filter_type, config_id)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        run_year(year=int(sys.argv[1]), filter_type=str(sys.argv[2]))
+    if len(sys.argv) == 4:
+        run_year(config_id=sys.argv[1], year=int(sys.argv[2]), filter_type=str(sys.argv[3]))
     else:
-        raise ValueError("Provide valid arguments. E.g.: python data_cube_filters.py <year> <filter_type>")
+        raise ValueError("Provide valid arguments. E.g.: python data_cube_filters.py <config_id> <year> <filter_type>")
