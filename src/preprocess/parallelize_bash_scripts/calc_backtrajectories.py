95c38210fa269f38ad5d72f5d265be3bf0fbe6d0
tabularaza27 <kai.jeggle@gmx.de>
2021-11-19 12:01:41 +0100

    refactor to class

--- src/preprocess/parallelize_bash_scripts/calc_backtrajectories.py
+++ src/preprocess/parallelize_bash_scripts/calc_backtrajectories.py
@@ -104,59 +104,59 @@ def run(n_workers, year):
     pc = ParallelCaltra(n_workers, year)
     pc.parallel_caltra()
 
-    # def parallel_caltra(n_workers, year, month):
-    #     """call bash script that calculates backtrajectories using lagranto in parallel using python multiprocessing
-    #
-    #     Args:
-    #         n_workers (int):
-    #         year (int):
-    #         month (int):
-    #
-    #     """
-    #     print("Start parallel merra preprocessing for month {} for year {} with {} workers".format(month, year,
-    #                                                                                                n_workers))
-    #
-    #     os.system("rm {}/*tmp_{}{:02d}*".format(OUT_FILE_DIR,year,month))
-    #     print("removed intermediate leftover files")
-    #
-    #     # link startfiles to output dir
-    #     # todo now I just link all available startfiles
-    #     os.system("ln -sf {}/* {}".format(START_FILE_DIR, OUT_FILE_DIR))
-    #
-    #     filepaths = glob.glob("{}/{}/*{}{:02d}*".format(START_FILE_DIR,year, year, month))
-    #
-    #     pool = mp.Pool(n_workers)
-    #     for filepath in filepaths:
-    #         date_hour = filepath.split("startf_")[1]
-    #         pool.apply_async(process_singlefile, args=(date_hour,))
-    #     pool.close()
-    #     pool.join()
+# def parallel_caltra(n_workers, year, month):
+#     """call bash script that calculates backtrajectories using lagranto in parallel using python multiprocessing
+#
+#     Args:
+#         n_workers (int):
+#         year (int):
+#         month (int):
+#
+#     """
+#     print("Start parallel merra preprocessing for month {} for year {} with {} workers".format(month, year,
+#                                                                                                n_workers))
+#
+#     os.system("rm {}/*tmp_{}{:02d}*".format(OUT_FILE_DIR,year,month))
+#     print("removed intermediate leftover files")
+#
+#     # link startfiles to output dir
+#     # todo now I just link all available startfiles
+#     os.system("ln -sf {}/* {}".format(START_FILE_DIR, OUT_FILE_DIR))
+#
+#     filepaths = glob.glob("{}/{}/*{}{:02d}*".format(START_FILE_DIR,year, year, month))
+#
+#     pool = mp.Pool(n_workers)
+#     for filepath in filepaths:
+#         date_hour = filepath.split("startf_")[1]
+#         pool.apply_async(process_singlefile, args=(date_hour,))
+#     pool.close()
+#     pool.join()
 
-    if __name__ == "__main__":
-        # python calc_backtrajectories.py --n_workers 8 --year 2008 #--months 1 2 3
-        # CLI = argparse.ArgumentParser()
-        # CLI.add_argument(
-        #     "--n_workers",
-        #     type=int,
-        #     default=6
-        # )
-        #
-        # CLI.add_argument(
-        #     "--year",
-        #     type=int
-        # )
-        # args = CLI.parse_args()
-        #
-        # n_workers = args.n_workers
-        # year = args.year
+if __name__ == "__main__":
+    # python calc_backtrajectories.py --n_workers 8 --year 2008 #--months 1 2 3
+    # CLI = argparse.ArgumentParser()
+    # CLI.add_argument(
+    #     "--n_workers",
+    #     type=int,
+    #     default=6
+    # )
+    #
+    # CLI.add_argument(
+    #     "--year",
+    #     type=int
+    # )
+    # args = CLI.parse_args()
+    #
+    # n_workers = args.n_workers
+    # year = args.year
 
-        n_workers=32
-        year=2008
+    n_workers=32
+    year=2008
 
-        print("n_workers: ", n_workers, "year: ", year)
-        run(n_workers, year)
+    print("n_workers: ", n_workers, "year: ", year)
+    run(n_workers, year)
 
-    # FILEPATHS=glob.glob("{}/{}/*{}*_*".format(OUT_FILE_DIR,year, year))
-    # BLOCKED_TIMES=[]
+# FILEPATHS=glob.glob("{}/{}/*{}*_*".format(OUT_FILE_DIR,year, year))
+# BLOCKED_TIMES=[]
 
-    # parallel_caltra(n_workers=n_workers, year=year)
+# parallel_caltra(n_workers=n_workers, year=year)
