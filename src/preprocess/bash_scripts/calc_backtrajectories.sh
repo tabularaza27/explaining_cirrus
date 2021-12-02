98f756dacaaa9634f421cad8325fff8137d68984
tabularaza27 <kai.jeggle@gmx.de>
2021-12-02 18:49:43 +0100

    create subdirectory for backtrajectory files

--- src/preprocess/bash_scripts/calc_backtrajectories.sh
+++ src/preprocess/bash_scripts/calc_backtrajectories.sh
@@ -15,14 +15,37 @@ model
 # input directory: ERA5 netcdf files
 era5filedir=/net/thermo/atmosdyn/era5/cdf
 
+# output directory
+outfiledir=/net/n2o/wolke_scratch/kjeggle/BACKTRAJECTORIES/outfiles
+
 # define start and end date
 dat=$1
 yyyy=`echo $dat | cut -c 1-4`
+mm=`echo $dat | cut -c 5-6`
+dd=`echo $dat | cut -c 7-8`
 backdat=$(newtime ${dat} -60)  # 2.5d backward
 
-# directory where netcdf files are linked and where trajectories will be written
 #cd /net/litho/atmosdyn/binderh/varia/kai/test
-cd /net/n2o/wolke_scratch/kjeggle/BACKTRAJECTORIES/outfiles
+
+# target dir, caltra script will be executed here
+# directory where netcdf files are linked and where trajectories will be written
+target_dir=${outfile}/{yyyy}/{mm}/{dd}
+
+# create target dir
+mkdir -p $target_dir
+cd $target_dir
+
+# link era5 files of the last 60 hours
+for i in {1..61}
+do
+  hourbackdat=$(newtime ${dat} - $i) # subtract one hour
+  yyyy=`echo $hourbackdat | cut -c 1-4`
+  mm=`echo $hourbackdat | cut -c 5-6`
+  dd=`echo $hourbackdat | cut -c 7-8`
+  hh=`echo $hourbackdat | cut -c 10-11`
+  ln -sf /net/thermo/atmosdyn/era5/cdf/${yyyy}/${mm}/*${yyyy}${mm}${dd}_hh
+done
+
 #ln -s /net/thermo/atmosdyn/era5/cdf/2008/01/
 
 # link era5 netcdf files to current working directory
@@ -33,35 +56,35 @@ cd /net/n2o/wolke_scratch/kjeggle/BACKTRAJECTORIES/outfiles
 #awk '{print $1,$2,$3/100}' startf_20080129_17_Pascal > startf_20080129_17
 
 ### calculate tra
-startf_file=${yyyy}/startf_${dat}
+startf_file=start_files/${yyyy}/startf_${dat}
 echo $startf_file
-caltra ${dat} ${backdat} $startf_file tra_tmp_${dat}.1 -j
-#caltra ${dat} ${backdat} /home/binderh/prog/varia/kai/startf_20080129_17 tra_tmp_${dat}.1 -j
-
-### add height to trajectories (in km)
-/home/binderh/prog/programs/height.to.traj/z2traj tra_tmp_${dat}.1 trah_tmp_${dat}.1
-
-### tra tracen
-if [ ! -f tracevars ];then
-    echo "T           1.    0    P" >> tracevars   # K
-    echo "Q        1000.    0    P" >> tracevars   # g/kg
-    echo "LWC   1000000.    0    P" >> tracevars   # mg/kg
-    echo "RWC   1000000.    0    P" >> tracevars   # mg/kg
-    echo "IWC   1000000.    0    P" >> tracevars   # mg/kg
-    echo "SWC   1000000.    0    P" >> tracevars   # mg/kg
-    echo "U           1.    0    P" >> tracevars   # m/s
-    echo "V           1.    0    P" >> tracevars   # m/s
-    echo "OMEGA       1.    0    P" >> tracevars   # Pa/s
-fi
-trace trah_tmp_${dat}.1 tra_traced_${dat}.1
-
-# append RHi
-#dim=`${LAGRANTO}/goodies/trainfo.sh ${tra_traced_${dat}.1} dim` #  dimensions of the trajectory file: #tra, #ntimes, #ncolumns
-#\rm -f rhi_to_tra.param
-#echo \"${inpfile}\"   > rhi_to_tra.param
-#echo \"${outfile}\"  >> rhi_to_tra.param
-#echo ${dim}          >> rhi_to_tra.param
-
-
-### clean
-rm tra_tmp_${dat}.1 trah_tmp_${dat}.1 # startf_${dat}.1
+#caltra ${dat} ${backdat} $startf_file tra_tmp_${dat}.1 -j
+##caltra ${dat} ${backdat} /home/binderh/prog/varia/kai/startf_20080129_17 tra_tmp_${dat}.1 -j
+#
+#### add height to trajectories (in km)
+#/home/binderh/prog/programs/height.to.traj/z2traj tra_tmp_${dat}.1 trah_tmp_${dat}.1
+#
+#### tra tracen
+#if [ ! -f tracevars ];then
+#    echo "T           1.    0    P" >> tracevars   # K
+#    echo "Q        1000.    0    P" >> tracevars   # g/kg
+#    echo "LWC   1000000.    0    P" >> tracevars   # mg/kg
+#    echo "RWC   1000000.    0    P" >> tracevars   # mg/kg
+#    echo "IWC   1000000.    0    P" >> tracevars   # mg/kg
+#    echo "SWC   1000000.    0    P" >> tracevars   # mg/kg
+#    echo "U           1.    0    P" >> tracevars   # m/s
+#    echo "V           1.    0    P" >> tracevars   # m/s
+#    echo "OMEGA       1.    0    P" >> tracevars   # Pa/s
+#fi
+#trace trah_tmp_${dat}.1 tra_traced_${dat}.1
+#
+## append RHi
+##dim=`${LAGRANTO}/goodies/trainfo.sh ${tra_traced_${dat}.1} dim` #  dimensions of the trajectory file: #tra, #ntimes, #ncolumns
+##\rm -f rhi_to_tra.param
+##echo \"${inpfile}\"   > rhi_to_tra.param
+##echo \"${outfile}\"  >> rhi_to_tra.param
+##echo ${dim}          >> rhi_to_tra.param
+#
+#
+#### clean
+#rm tra_tmp_${dat}.1 trah_tmp_${dat}.1 # startf_${dat}.1
