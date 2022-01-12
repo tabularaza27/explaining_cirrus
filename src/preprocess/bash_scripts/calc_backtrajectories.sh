dc8330ae1a12aca9a860bd6853a1998668e5d8a9
tabularaza27 <kai.jeggle@gmx.de>
2022-01-12 16:07:16 +0100

    added o3 and cc as tracers

--- src/preprocess/bash_scripts/calc_backtrajectories.sh
+++ src/preprocess/bash_scripts/calc_backtrajectories.sh
@@ -79,6 +79,8 @@ if [ ! -f tracevars ];then
     echo "U           1.    0    P" >> tracevars   # m/s
     echo "V           1.    0    P" >> tracevars   # m/s
     echo "OMEGA       1.    0    P" >> tracevars   # Pa/s
+    echo "o3       1000     0    O" >> tracevats   # g/kg
+    echo "cc          1.    0    O" >> tracevats   # percentage
 fi
 trace trah_tmp_${dat}.1 tra_traced_${dat}.1
 #
